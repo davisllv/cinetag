3d70fb4c2a186c682a91e2c34c5f7eaf7f4b3b6b
davisllv <davisllvsnt57@gmail.com>
2023-11-21 23:45:14 -0300

    finish the footer

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,3 +1,4 @@
+import Footer from "components/Footer";
 import Header from "components/Header";
 
 const Inicio = () => {
@@ -5,6 +6,7 @@ const Inicio = () => {
     <>
       <Header />
       <h1>Olá Mundo Estou em uma Página Nova</h1>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import Style from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={Style.footer}>
+      <span>Desenvolvido por Alura!</span>
+    </footer>
+  );
+};
+
+export default Footer;
